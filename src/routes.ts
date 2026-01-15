7cad4f79a149973f39a137816e4b897998b89648
Luiz-Mendonca1 <luizeduardomendonca77@gmail.com>
2026-01-15 10:21:11 -0300

    Aplicando método PUT,  DELETE

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,17 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 
 const task=['task1','task2','task3'];
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+
+  console.log("Acessou a rota: " + req.url);
+
+  next();
+});
+
+// listar tarefas
 
 router.get("/task", (req: Request, res: Response) => {
 
@@ -23,8 +32,29 @@ router.post("/task", (req: Request, res: Response) => {
 
   const { name } = req.body;
 
-  console.log(name);
+  task.push(name);
 
   res.json({ ok: true });
+});
+
+// atualizar tarefa
+router.put("/task/:id", (req: Request, res: Response) => {
+
+  const {id} = req.params;
+  const { name } = req.body;
+
+  task[Number(id)] = name;
+
+  res.json(task)
+}); 
+
+// deletar tarefa
+router.delete("/task/:id", (req: Request, res: Response) => {
+
+  const {id} = req.params;
+  
+  task.splice(Number(id),1);
+
+  res.json(task)
 });
 export default router;
