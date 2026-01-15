f83541c399de97f3c02870e545d3ab811dcf987c
Luiz-Mendonca1 <luizeduardomendonca77@gmail.com>
2026-01-15 09:54:47 -0300

    Request Body

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,29 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
+const task=['task1','task2','task3'];
+
 router.get("/task", (req: Request, res: Response) => {
 
-  const nome = req.query.nome;
+  res.json(task);
 
-  res.json({ message: `Hello, ${nome || "world"}!` });
 });
 
+router.get("/task/:id", (req: Request, res: Response) => {
+
+  const id = req.params.id;
+
+  res.json({task: task[Number(id)]});
+});
+
+// cadastrar
+
+router.post("/task", (req: Request, res: Response) => {
+
+  const { name } = req.body;
+
+  console.log(name);
+
+  res.json({ ok: true });
+});
 export default router;
